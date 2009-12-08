6dbe86ec92b1bdb6075557a628d525f830518529
ssp <ssp-web@earthlingsoft.net>
2009-12-08 16:41:23 +0100

    Paste License blurb into new files.

--- Plugin/CTFLoader.m
+++ Plugin/CTFLoader.m
@@ -1,7 +1,4 @@
-/*
- CTFImage.m
- ClickToFlash
- 
+/* 
  The MIT License
  
  Copyright (c) 2009 ClickToFlash Developers
@@ -23,7 +20,7 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
- */ 
+*/ 
 
 #import "CTFLoader.h"
 #import "CTFUtilities.h"
